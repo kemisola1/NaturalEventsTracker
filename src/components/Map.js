e7e6b952989251b440fe506c34adcb5525654349
Oluwakemi <jeleeloluwakemi@gmail.com>
2023-08-22 12:09:34 +0100

    committing codes

---
+++ src/components/Map.js
@@ -0,0 +1,41 @@
+import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
+import { useState } from 'react'
+
+
+const Map = ({ eventData, center, zoom}) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
+
+    const markers = eventData.map(ev => {
+        if(ev.categories[0].id === 8) {
+           return <LocationMarker lat={ev.geometries[0].coordinates[1]} 
+           lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+        }
+        return null
+    })
+
+  return (
+    <div className='map'>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: ''}}
+          defaultCenter={ center }
+          defaultZoom={ zoom }
+        > 
+            {markers}
+        </GoogleMapReact>
+        {locationInfo && <LocationInfoBox info={locationInfo} />}
+        </div>
+  )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 9.0820,
+        lng: 8.6753
+    },
+    zoom: 3
+}
+
+export default Map
